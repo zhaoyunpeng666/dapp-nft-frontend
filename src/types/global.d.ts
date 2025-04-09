8104fcf4ad17e262ac9b4c8dd1984b67f98a1bb6
zhaoyunpeng <2228144965@qq.com>
2025-04-09 11:05:52 +0800

    feat: 更新tsconfig.json中的jsx编译选项为react-jsx，并在global.d.ts中新增input和video元素的类型定义，确保React类型全局可用

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -12,6 +12,12 @@ declare global {
       span: React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>;
       select: React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>;
       option: React.DetailedHTMLProps<React.OptionHTMLAttributes<HTMLOptionElement>, HTMLOptionElement>;
+      input: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+      video: React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>;
     }
+  }
+  // 导出全局React命名空间
+  export namespace React {
+    // 确保React类型已经全局可用
   }
 } 
